2ec97d5a5e0670414d101acf85b02192f0068fe7
videvops <development@visolutions.com.mx>
2023-02-09 03:39:31 +0000

    Habilitacion catalogos planta y maquina

--- src/home/Home.js
+++ src/home/Home.js
@@ -19,10 +19,10 @@ import ListadoParos from "../components/listadoParo/ListadoParos";
 import CatalogoAreas from "../pages/Catalogos/Areas/CatalogoAreas";
 import CatalogoTurnos from "../pages/Catalogos/Turnos/CatalogoTurnos";
 import CatalogoLineas from "../pages/Catalogos/Lineas/CatalogoLineas";
-// import CatalogoPlantas from "../pages/Catalogos/Plantas/CatalogoPlantas";
+import CatalogoPlantas from "../pages/Catalogos/Plantas/CatalogoPlantas";
 // import CatalogoUsuarios from "../pages/Catalogos/Usuarios/CatalogoUsuarios";
 import CatalogoProducto from "../pages/Catalogos/Productos/CatalogoProducto";
-// import CatalogoMaquinas from "../pages/Catalogos/Maquinas/CatalogoMaquinas";
+import CatalogoMaquinas from "../pages/Catalogos/Maquinas/CatalogoMaquinas";
 // import CatalogoTipoParo from "../pages/Catalogos/TiposParo/CatalogoTipoParo";
 // import CatalogoDirecciones from "../pages/Catalogos/Direcciones/CatalogoDirecciones";
 
@@ -210,13 +210,13 @@ const Home = ({ setLogueado }) => {
                         { label: "Áreas", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoAreas" },
                         // { label: "Direcciones", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoDirecciones" },
                         { label: "Líneas", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoLineas" },
-                        // { label: "Plantas", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoPlantas" },
+                        { label: "Plantas", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoPlantas" },
                         { label: "Productos", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoProductos" },
                         // { label: "Roles", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoRoles" },
                         // { label: "TipoParo", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoTipoParo" },
                         { label: "Turnos", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoTurnos" },
                         // { label: "Usuarios", icon: "pi pi-fw pi-table text-blue-500", to: "/catalogoUsuarios" },
-                        // {label: "Maquina",icon: "pi pi-fw pi-bookmark text-blue-500",to: "/catalogoMaquinas",},
+                        {label: "Maquinas",icon: "pi pi-fw pi-bookmark text-blue-500",to: "/catalogoMaquinas",},
                         // {label: "Modo de Falla",icon: "pi pi-fw pi-bookmark text-blue-500",},
                     ],
                 },
@@ -280,13 +280,13 @@ const Home = ({ setLogueado }) => {
                     <Route path="/catalogoAreas" component={CatalogoAreas} />
                     {/* <Route path="/catalogoDirecciones" component={CatalogoDirecciones} /> */}
                     <Route path="/catalogoLineas" component={CatalogoLineas} />
-                    {/* <Route path="/catalogoPlantas" component={CatalogoPlantas} /> */}
+                    <Route path="/catalogoPlantas" component={CatalogoPlantas} />
                     <Route path="/catalogoProductos" component={CatalogoProducto} />
                     {/* <Route path="/catalogoRoles" component={CatalogoRoles} /> */}
                     {/* <Route path="/catalogoTipoParo" component={CatalogoTipoParo} /> */}
                     <Route path="/catalogoTurnos" component={CatalogoTurnos} />
                     {/* <Route path="/catalogoUsuarios" component={CatalogoUsuarios} /> */}
-                    {/* <Route path="/catalogoMaquinas" component={CatalogoMaquinas} /> */}
+                    <Route path="/catalogoMaquinas" component={CatalogoMaquinas} />
                     <Route path="/desperdicio" component={Desperdicio} />
                     <Route path="/monitorDeLineas" component={()=><MonitorDeLineas tiempoReal = {false}/>}/>
                     <Route path="/monitorDeLineasTiempoReal" component={()=><MonitorDeLineas tiempoReal = {true}/>}/>
