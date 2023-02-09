294588049e79bfda0c567f7985806007a886276a
videvops <development@visolutions.com.mx>
2023-02-09 02:56:55 +0000

    Catalogo Lineas

--- src/pages/Catalogos/Lineas/Dialogos/CrearModificar.js
+++ src/pages/Catalogos/Lineas/Dialogos/CrearModificar.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react'
+import Axios from 'axios';
+import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
 import { Dropdown } from 'primereact/dropdown';
 import { InputText } from 'primereact/inputtext';
-import { productDialogFooter } from '../../ComponentsCat/Botones/CrearRegistro';
-import Axios from 'axios';
 import { statusDisponibles } from '../../ComponentsCat/Constantes/constantes';
+import { TextoAdvertencia, MensajeAdvertencia } from '../../ComponentsCat/Mensajes/Mensajes';
+
 import Environment from '../../../../Environment';
 
 const getRoute = Environment()
@@ -26,45 +28,68 @@ const CrearModificar = ({
     //---> Plantas
     const [plantasDisponibles,setPlantasDisponibles]=useState([])
     useEffect(() => {
-        Axios.get(getRoute+"/plantas/list").then(res=>setPlantasDisponibles(res.data))
+        Axios.get(getRoute + "/plantas/list").then(res => setPlantasDisponibles(res.data))
     }, [])
     //---> Areas
     const [areasDisponibles, setAreasDisponibles]=useState([])
     useEffect(() => {
         if(product.idPlanta!==''){
-        Axios.get(getRoute+`/areas/planta/${product.idPlanta}`).then(res=>setAreasDisponibles(res.data))
+            Axios.get(getRoute + `/areas/planta/${product.idPlanta}`).then(res => setAreasDisponibles(res.data))
         }
     }, [product.idPlanta])
 
 //--------------------| Validar campos  |--------------------
-    // const [validarNombre,setValidarNombre]=useState("");                // Validar nombre de planta
-    // const [boton,setBoton]=useState(false);                             // Activar o desactivar boton
-    const Advertencia=(<p style={{color:"red"}}>Campo no valido</p>);   // Mensaje de advertencia
-    const expresion=/^[a-zA-Z0-9._-\s]{1,40}$/;                            // Solo nombres y numeros
-
+    const [datosInvalidos, setDatosInvalidos] = useState(false)     // Validar envio
+    const [mensaje, setMensaje] = useState("")                      // Mensaje para envio
+    const [texto, setTexto] = useState("")                          // Mensaje para input
+    const expresion=/^[a-zA-Z0-9._-\s]{1,40}$/;                     // Todo menos ','
+    //--> Validar input
     const Verificar=(texto)=>{
         if (!expresion.test(texto)){
-            setValidarNombre("p-invalid");
-            setBoton(true);
+            setValidarNombre("p-invalid")   // Input rojo
+            setTexto("Campo invalido")      // Texto de advertencia de input
+            setBoton(true);                 // Validacion para input invalido
         }else{
-            setValidarNombre("");
-            setBoton(false);
+            setValidarNombre("")
+            setTexto("")
+            setBoton(false)
         }
     }
-
-    // useEffect(() => { 
-    //     if (Object.values(product).includes("")) {
-    //         console.log("esta vacio")
-    //         setBoton(true)
-    //     } else {
-    //         console.log("ya no esta vacio")
-    //         setBoton(false)
-    //     }
-    // }, [product])
-
-//--------------------| Botones de confirmacion |--------------------
-    //------> Botones para crear registro
-    const crearRegistro = productDialogFooter(hideDialog, saveProduct, boton, product);
+    //--> Validar datos antes de envio
+    const enviarDatos = () => {
+        if (Object.values(product).includes("")) {
+            setMensaje("Todos los campos son obligatorios")
+            setDatosInvalidos(true)
+            setTimeout(() => {
+                setDatosInvalidos(false)
+            }, 3000);
+            return
+        }
+        else {
+            if (boton) {
+                setMensaje("El nombre no es valido")
+                setDatosInvalidos(true)
+                setTimeout(() => {
+                    setDatosInvalidos(false)
+                }, 3000);
+                return
+            }
+        }
+        saveProduct()
+    }
+    //--> Botones de accion
+    const botones = () => {
+        return (
+            <>
+                <Button
+                    label="Cancelar" icon="pi pi-times" className="p-button-rounded" onClick={hideDialog}
+                />                
+                <Button
+                    label="Guardar" icon="pi pi-check" className="p-button-rounded" onClick={enviarDatos}
+                />
+            </>
+        )
+    }
 
 //--------------------| Valor que regresara  |--------------------
     return (
@@ -74,50 +99,48 @@ const CrearModificar = ({
         header={titulos.VentanaCrear} 
         modal 
         className="p-fluid" 
-        footer={crearRegistro} 
+        footer={botones} 
         onHide={hideDialog}
         >
             <div className="field">
                 <label>Planta</label>
                 <Dropdown 
-                optionLabel="planta" 
-                optionValue="id" 
-                value={product.idPlanta} 
-                options={plantasDisponibles} 
-                onChange={(e) => {updateField(e.value, "idPlanta")}} 
-                placeholder="--Selecciona una planta--"
+                    optionLabel="planta" 
+                    optionValue="id" 
+                    value={product.idPlanta} 
+                    options={plantasDisponibles} 
+                    onChange={(e) => {updateField(e.value, "idPlanta")}} 
+                    placeholder="--Selecciona una planta--"
                 />
             </div>
             <div className="field">
                 <label>Area</label>
                 <Dropdown 
-                optionLabel="area" 
-                optionValue="id" 
-                value={product.idArea} 
-                options={areasDisponibles} 
-                onChange={(e) => {updateField(e.value, "idArea")}} 
-                placeholder="--Selecciona una area--"
+                    optionLabel="area" 
+                    optionValue="id" 
+                    value={product.idArea} 
+                    options={areasDisponibles} 
+                    onChange={(e) => {updateField(e.value, "idArea")}} 
+                    placeholder="--Selecciona una area--"
                 />
             </div>
             <div className="field">
-                <label 
-                htmlFor="linea"                                   // CAMBIAR...
-                >
+                <label htmlFor="linea">
                     Linea
                 </label>
                 <InputText 
-                id="linea"                                        // CAMBIAR...
-                value={product.linea}                             // CAMBIAR...
-                onChange={(e) => {
-                    updateField(e.target.value, "linea");  // CAMBIAR...
-                    Verificar(e.target.value)
-                }} 
-                required 
-                autoFocus 
-                className={validarNombre}
-                maxLength="30" 
+                    id="linea"                                      // CAMBIAR...
+                    value={product.linea}                           // CAMBIAR...
+                    onChange={(e) => {
+                        updateField(e.target.value, "linea");       // CAMBIAR...
+                        Verificar(e.target.value)
+                    }} 
+                    required 
+                    autoFocus 
+                    className={validarNombre}
+                    maxLength="30" 
                 />
-                {boton && Advertencia}
+                {boton && <TextoAdvertencia>{texto}</TextoAdvertencia>}
             </div>
             {!tieneId && (<div className="field">
                 <label>Status</label>
@@ -131,6 +154,7 @@ const CrearModificar = ({
                     placeholder="--Selecciona un status--"
                 />
             </div>)}
+            {datosInvalidos && <MensajeAdvertencia>{mensaje}</MensajeAdvertencia>}
         </Dialog>
     )
 }
