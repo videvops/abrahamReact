eacbcdb1a333e735f36c3d8eab4963c01269e4a9
videvops <development@visolutions.com.mx>
2023-03-01 10:49:05 -0600

    Reportes Desperdicio, filtro de tablas desperdicio y listado de paros

--- src/components/desperdicio/Tabla/TablaDesperdicio.js
+++ src/components/desperdicio/Tabla/TablaDesperdicio.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import { Column } from "primereact/column";
-import TablaDesing from '../UI/DiseñoTabla'
+import React, { useState } from 'react';
+import { FilterMatchMode } from 'primereact/api';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import { CardGeneral, CardTabla } from '../UI/Cards'
 
 const TablaDesperdicio = ({registros}) => {
+
+
+    const [filters, setFilters] = useState({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        planta: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        linea: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        area: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        maquina: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        rechazo: { value: null, matchMode: FilterMatchMode.CONTAINS }
+    });
+
+
     if(Object.entries(registros).length === 0){
         return(<h3>No hay informacion disponible</h3>)
     }else{
@@ -11,14 +24,25 @@ const TablaDesperdicio = ({registros}) => {
             <div className='col-12 md:col-12 grid p-fluid'>
                 <CardGeneral> 
                     <CardTabla>
-                        <TablaDesing datos={registros} >
-                            <Column field="fecha" header="Fecha" style={{ textAlign: "center", minWidth: '12rem' }} sortable />
-                            <Column field="planta" header="Planta" style={{ textAlign: "center" }} sortable/>
-                            <Column field="area" header="Area" style={{ textAlign: "center" }} sortable/>
-                            <Column field="linea" header="Linea" style={{ textAlign: "center" }} sortable/>
-                            <Column field="maquina" header="Maquina" style={{ textAlign: "center" }} sortable/>
-                            <Column field="rechazo" header="Rechazos[Kg]" style={{ textAlign: "center" }} sortable/>
-                        </TablaDesing>
+                        <DataTable
+                            value={registros} 
+                            responsiveLayout="scroll" 
+                            style={{ fontSize: "20px", textAlign: "center" }}
+                            paginator
+                            rows={5}
+                            filters={filters}
+                            filterDisplay="row"
+                            rowsPerPageOptions={[5, 10, 25]}
+                            globalFilterFields={['planta', 'area', 'linea','maquina','rechazo']} 
+                            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                            currentPageReportTemplate="Registros de {first} a {last} de {totalRecords} registros">
+                            <Column field="fecha" header="Fecha" style={{ textAlign: "center",width:"15%"}}  />
+                            <Column field="planta" header="Planta" filter filterPlaceholder="Buscar por planta" filterMenuStyle={{ width: '20%' }} style={{ textAlign: "center",width:"20%" }} />
+                            <Column field="area" header="Area" filter filterPlaceholder="Buscar por Area"  filterMenuStyle={{ width: '20%' }} style={{ textAlign: "center",width:"20%" }} />
+                            <Column field="linea" header="Linea" filter filterPlaceholder="Buscar por Linea"  filterMenuStyle={{ width: '20%' }} style={{ textAlign: "center",width:"20%" }} />
+                            <Column field="maquina" header="Maquina" filter filterPlaceholder="Buscar por Maquina"   filterMenuStyle={{ width: '20%' }} style={{ textAlign: "center",width:"20%" }} />
+                            <Column field="rechazo" header="Rechazos[Kg]" filter filterPlaceholder="Buscar por Rechazo"  filterMenuStyle={{ width: '10%' }} style={{ textAlign: "center" ,width:"10%"}} />
+                        </DataTable>
                     </CardTabla>
                 </CardGeneral>
             </div>
