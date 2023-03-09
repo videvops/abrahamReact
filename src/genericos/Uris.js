b76a33eaa8c90877db7a9ac3ba408824efe807c2
videvops <development@visolutions.com.mx>
2023-03-09 15:57:19 -0600

    Monitor de lineas tiempo real

--- src/genericos/Uris.js
+++ src/genericos/Uris.js
@@ -5,6 +5,8 @@ export const PLANTAS_GET_COMBO = "plantas/list";
 export const PLANTAS_AREAS = "areas/plantas";
 // LINEAS
 export const LINEAS_AREAS ="lineas/areas";
+// MAQUINAS
+export const MAQUINAS_GET_COMBO ="maquinas/list";
 
 //BITACORA
 export const BITACORA_POST_FILTER = "bitacora/filter";
@@ -23,8 +25,16 @@ export const LISTADO_DE_PAROS_REPORTE = "reportes/listadoParos";
 //MONITOR DE LINEA
 export const ULTIMOS_PAROS = "paros/ultimosParos/linea";
 export const TOP_FIVE ="paros/topFive/linea";
+export const INDICADORES_MONITOR_LINEA ="indicadores/monitor/linea";
 
 // CATALOGO PRODUCTOS
 export const RECUPERAR_DATOS_PRODUCTO = "productos/getById";
 export const ENVIAR_PARTE1_PRODUCTOS = "productos";
 export const ENVIAR_PARTE2_PRODUCTOS = "productos/config/velocidades";
+
+// UTILERIAS 
+export const GET_FECHAS_FOR_LIVE_SCREEN = "utilerias/getFechasForLiveScreen";
+
+// DESPERDICIOS
+
+export const DESPERDICIO_TABLE ="desperdicio/table/rechazos";
